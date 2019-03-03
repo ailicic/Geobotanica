43b4849450f39c5dff85fa6309a2a7f2b880de6f
Alan Ilicic <ailicic@posteo.de>
2019-03-03 01:25:35 -0800

    Simplified PlantNameSearchService handling of single/multi word searches.

--- app/src/main/java/com/geobotanica/geobotanica/data_taxa/util/PlantNameSearchService.kt
+++ app/src/main/java/com/geobotanica/geobotanica/data_taxa/util/PlantNameSearchService.kt
@@ -10,7 +10,6 @@ import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.Job
-import kotlinx.coroutines.channels.ProducerScope
 import kotlinx.coroutines.channels.ReceiveChannel
 import kotlinx.coroutines.channels.produce
 import javax.inject.Inject
@@ -18,7 +17,6 @@ import kotlin.coroutines.CoroutineContext
 import kotlin.system.measureTimeMillis
 
 
-// TODO: Figure out how to merge single/double word searches
 class PlantNameSearchService @Inject constructor (
         private val taxonRepo: TaxonRepo,
         private val vernacularRepo: VernacularRepo
@@ -28,127 +26,78 @@ class PlantNameSearchService @Inject constructor (
 
     enum class PlantNameType { SCIENTIFIC, VERNACULAR }
 
-    private data class SingleWordSearch(
-            val function: (String, Int) -> List<Long>?,
+    private data class PlantNameSearch(
             val plantNameType: PlantNameType,
+            val fun1: ( (String, Int) -> List<Long>? )? = null,
+            val fun2: ( (String, String, Int) -> List<Long>? )? = null,
             val isStarred: Boolean = false
     ) {
         val functionName: String
             get() {
-                return function.toString()
+                return (fun1 ?: fun2).toString()
                     .removePrefix("function ")
                     .removeSuffix(" (Kotlin reflection is not available)")
             }
     }
 
-    private data class DoubleWordSearch(
-            val function: (String, String, Int) -> List<Long>?,
-            val plantNameType: PlantNameType,
-            val isStarred: Boolean = false
-    ) {
-        val functionName: String
-            get() {
-                return function.toString()
-                        .removePrefix("function ")
-                        .removeSuffix(" (Kotlin reflection is not available)")
-            }
-    }
-
     private val singleWordSearchSequence = listOf(
-            SingleWordSearch(vernacularRepo::starredStartsWith, VERNACULAR, isStarred = true),
-            SingleWordSearch(taxonRepo::starredStartsWith, SCIENTIFIC, isStarred = true),
-            SingleWordSearch(vernacularRepo::nonFirstWordStartsWith, VERNACULAR),
-            SingleWordSearch(vernacularRepo::firstWordStartsWith, VERNACULAR),
-            SingleWordSearch(taxonRepo::genericStartsWith, SCIENTIFIC),
-            SingleWordSearch(taxonRepo::epithetStartsWith, SCIENTIFIC)
+        PlantNameSearch(VERNACULAR, fun1 = vernacularRepo::starredStartsWith, isStarred = true),
+        PlantNameSearch(SCIENTIFIC, fun1 = taxonRepo::starredStartsWith, isStarred = true),
+        PlantNameSearch(VERNACULAR, fun1 = vernacularRepo::nonFirstWordStartsWith),
+        PlantNameSearch(VERNACULAR, fun1 = vernacularRepo::firstWordStartsWith),
+        PlantNameSearch(SCIENTIFIC, fun1 = taxonRepo::genericStartsWith),
+        PlantNameSearch(SCIENTIFIC, fun1 = taxonRepo::epithetStartsWith)
     )
 
     private val doubleWordSearchSequence = listOf(
-            DoubleWordSearch(vernacularRepo::starredStartsWith, VERNACULAR, isStarred = true),
-            DoubleWordSearch(taxonRepo::starredStartsWith, SCIENTIFIC, isStarred = true),
-            DoubleWordSearch(vernacularRepo::anyWordStartsWith, VERNACULAR),
-            DoubleWordSearch(taxonRepo::genericOrEpithetStartsWith, SCIENTIFIC)
+        PlantNameSearch(VERNACULAR, fun2 = vernacularRepo::starredStartsWith, isStarred = true),
+        PlantNameSearch(SCIENTIFIC, fun2 = taxonRepo::starredStartsWith, isStarred = true),
+        PlantNameSearch(VERNACULAR, fun2 = vernacularRepo::anyWordStartsWith),
+        PlantNameSearch(SCIENTIFIC, fun2 = taxonRepo::genericOrEpithetStartsWith)
     )
 
     data class SearchResult(
-            val id: Long,
-            val plantNameType: PlantNameType,
-            var isStarred: Boolean,
-            val name: String
+        val id: Long,
+        val plantNameType: PlantNameType,
+        var isStarred: Boolean,
+        val name: String
     )
 
     fun getAllStarred(): List<SearchResult> {
-        val starred = mutableListOf<SearchResult>()
-        starred.addAll(taxonRepo.getAllStarred().map {
-            mapIdToSearchResult(it, SCIENTIFIC, isStarred = true)
-        })
-        starred.addAll(vernacularRepo.getAllStarred().map {
-            mapIdToSearchResult(it, VERNACULAR, isStarred = true)
-        })
-        return starred
+        return mutableListOf<SearchResult>().apply {
+            addAll(taxonRepo.getAllStarred().map { mapIdToSearchResult(it, SCIENTIFIC, isStarred = true) })
+            addAll(vernacularRepo.getAllStarred().map { mapIdToSearchResult(it, VERNACULAR, isStarred = true) })
+        }
     }
 
     @ExperimentalCoroutinesApi
     fun search(searchText: String): ReceiveChannel<List<SearchResult>> = produce {
-        if (searchText.isEmpty()) {
-            send(emptyList())
-            close()
-            return@produce
-        }
-
         val words = searchText.split(' ').filter { it.isNotBlank() }
+        val isSingleWord = words.size == 1
         Lg.d("Search words: $words")
 
-        when {
-            words.size == 1 -> performSingleWordSearch(words[0])
-            words.size > 1 -> performDoubleWordSearch(words[0], words[1])
+        val starredFilter = StarredFilter()
+        val combinedResults = mutableListOf<SearchResult>() // Is ordered by wordSearchSequence
+        val searchSequence = if (isSingleWord) singleWordSearchSequence else doubleWordSearchSequence
+        searchSequence.forEach forEachSearch@{ search ->
+            if (combinedResults.size >= DEFAULT_RESULT_LIMIT)
+                return@forEachSearch
+
+            val time = measureTimeMillis {
+                val limit = getLimit(combinedResults)
+                var results =
+                        if (isSingleWord) search.fun1!!(words[0], limit) ?: return@forEachSearch
+                        else search.fun2!!(words[0], words[1], limit) ?: return@forEachSearch
+                results = starredFilter.filter(results, search.isStarred, search.plantNameType)
+                combinedResults.addAll(
+                        results.map { id: Long -> mapIdToSearchResult(id, search.plantNameType, search.isStarred) }
+                                .distinctBy { it.name }
+                )
+            }
+            Lg.d("${search.functionName}: ${combinedResults.size} hits ($time ms)")
+            send(combinedResults) // Send partial results as they become available
         }
         close()
-    }
-
-    @ExperimentalCoroutinesApi
-    private suspend fun ProducerScope<List<SearchResult>>.performSingleWordSearch(word: String) {
-        val starredFilter = StarredFilter()
-        val combinedResults = mutableListOf<SearchResult>() // Is ordered by wordSearchSequence
-        singleWordSearchSequence.forEach forEachSearch@{ search ->
-            if (combinedResults.size >= DEFAULT_RESULT_LIMIT)
-                return@forEachSearch
-
-            val time = measureTimeMillis {
-                var results = search.function(word, getLimit(combinedResults)) ?: return@forEachSearch
-                results = starredFilter.filter(results, search.isStarred, search.plantNameType)
-                combinedResults.addAll(
-                    results.map { id: Long -> mapIdToSearchResult(id, search.plantNameType, search.isStarred) }
-                        .distinctBy { it.name }
-
-                )
-            }
-            Lg.d("${search.functionName}: ${combinedResults.size} hits ($time ms)")
-            send(combinedResults) // Send partial results as they become available
-        }
-    }
-
-    @ExperimentalCoroutinesApi
-    private suspend fun ProducerScope<List<SearchResult>>.performDoubleWordSearch(first: String, second: String) {
-        val starredFilter = StarredFilter()
-        val combinedResults = mutableListOf<SearchResult>() // Is ordered by wordSearchSequence
-        doubleWordSearchSequence.forEach forEachSearch@{ search ->
-            if (combinedResults.size >= DEFAULT_RESULT_LIMIT)
-                return@forEachSearch
-
-            val time = measureTimeMillis {
-                var results = search.function(first, second, getLimit(combinedResults)) ?: return@forEachSearch
-                results = starredFilter.filter(results, search.isStarred, search.plantNameType)
-
-                combinedResults.addAll(
-                        results.map { id: Long ->
-                            mapIdToSearchResult(id, search.plantNameType, isStarred = search.isStarred)
-                        }
-                )
-            }
-            Lg.d("${search.functionName}: ${combinedResults.size} hits ($time ms)")
-            send(combinedResults) // Send partial results as they become available
-        }
     }
 
     private fun getLimit(combinedResults: MutableList<SearchResult>) =
@@ -160,7 +109,6 @@ class PlantNameSearchService @Inject constructor (
             SCIENTIFIC -> taxonRepo.get(id)!!.latinName.capitalize()
         })
     }
-
 
     class StarredFilter {
         private val starredTaxaIds = mutableListOf<Long>()
